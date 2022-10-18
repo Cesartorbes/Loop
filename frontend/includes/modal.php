9166fc881497feb69aefb09d5b4b2e63693ea036
Gustavo Orth <gustavo.241004@bento.local>
2022-10-18 16:41:37 -0300

    modal perfil advanced pack

--- frontend/includes/modal.php
+++ frontend/includes/modal.php
@@ -1,31 +1,17 @@
-<?php
-echo '<div class="modal fade" id="modal-perfil" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal fade" id="modal-perfil" tabindex="-1" role="dialog" aria-hidden="true">
 <div class="modal-dialog" role="document">
-    <div class="modal-content">
+    <div class="modal-content bg-dark">
         <div class="modal-header">
-            <h5 class="modal-title sb-txt-secondary sb-w-700">
-                Perfil | <a data-bs-toggle="modal"
-                data-bs-target="#modal-agendamentos" href="#" > Agendamento </a>
-            </h5>
-            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Fechar">
+            <h5 class="modal-title sb-txt-primary sb-w-700">
+                Registre-se
+            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
                 <span aria-hidden="true" class="sb-txt-white">
                     &times;
                 </span>
             </button>
         </div>
         <div class="modal-body">
-            <form id="form-perfil" method="POST" action="/elsalvador/atualizarperfil.php">';
-        
-            if(isset($_SESSION['user_id'])){
-                $conn1 = mysqli_connect("localhost", "root", "", "barbearia_dev");
-
-                $queryUsuarioLogado = "CALL PROC_SEL_USUARIO({$_SESSION['user_id']})";
-
-                $resultUsuarioLogado = mysqli_query($conn1, $queryUsuarioLogado);
-
-                $dadosUsuarioLogado = mysqli_fetch_assoc($resultUsuarioLogado);
-
-                echo "
+            <form id="form-perfil" method="POST" action="/elsalvador/atualizarperfil.php">
                     <!-- Nome -->
                     <div class='form-group row'>
                         <label for='inputPassword' class='col-sm-4 col-form-label sb-txt-white'>
@@ -34,11 +20,9 @@ echo '<div class="modal fade" id="modal-perfil" tabindex="-1" role="dialog" aria
                         <div class='col-sm-8'>
                             <input 
                                 type='text' 
-                                class='form-control-plaintext sb-form-input pl-2 cursor-na' 
-                                value='{$dadosUsuarioLogado['nome']}'
+                                class='form-control-plaintext sb-form-input pl-2 bg-danger cursor-na' 
                                 name='nome'
                                 id='input-nome'
-                                readonly
                             >
                         </div>
                     </div>
@@ -50,33 +34,28 @@ echo '<div class="modal fade" id="modal-perfil" tabindex="-1" role="dialog" aria
                         <div class='col-sm-8'>
                             <input 
                                 type='text' 
-                                class='form-control-plaintext sb-form-input pl-2 maskTelefone cursor-na' 
-                                value='{$dadosUsuarioLogado['telefone']}'
+                                class='form-control-plaintext sb-form-input pl-2 bg-danger maskTelefone cursor-na' 
                                 name='telefone'
                                 id='input-telefone'
-                                readonly
                             >
                         </div>
                     </div>
-                ";}
-                echo '<div class="btn-salvar">
+                    <div class="btn-salvar">
                 <button class="btn sb-btn-secondary sb-w-700 d-none" id="salvar">Salvar Alterações</button>
             </div>
         </form>
     </div>
     <div class="modal-footer">
-        <button type="button" class="btn btn-secondary sb-w-700" data-bs-dismiss="modal">
+        <button type="button" class="btn btn-secondary sb-w-700" data-dismiss="modal">
             Fechar
         </button>
         <button 
             class="btn sb-btn-secondary sb-w-700"
             id="editar"
         >
-            Editar
+            Salvar
         </button>
     </div>
 </div>
 </div>
-</div>';
-
-?>
+</div>
